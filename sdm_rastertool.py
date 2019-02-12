b5e77fe04e2eaf8d62cdca5b3f3b44ee9a53d143
Helen Davis <htdavis@colostate.edu>
2019-02-12 13:57:04 -0700

    add snap raster, updated user prompts

--- sdm_rastertool.py
+++ sdm_rastertool.py
@@ -14,9 +14,9 @@ import time
 arcpy.env.overwriteOutput=True
 start=time.time()
 
-## Enter workspace. Workspace should be geodatabase of all rasters.
+## Enter workspace. Workspace should be geodatabase of all rasters you want resampled, etc.
 
-arcpy.env.workspace = raw_input('Paste the path to the workspace and hit enter\n') 
+arcpy.env.workspace = raw_input('Enter path to geodatabase\n') 
 
 ## check workspace and raster files
 rasterlist=arcpy.ListRasters("*")
@@ -24,22 +24,25 @@ print "Raster List"
 for raster in rasterlist:
     print(raster)
 
-## check workspace and shapefile mask, define mask
+## user chooses raster with desired extent, projection, and resolution
+snap=raw_input('Enter path to raster with desired extent, projection, and resolution\n') 
+
 featurelist=arcpy.ListFeatureClasses("*")
 print "Shapefile List"
 for feature in featurelist:
     print feature
-mask=raw_input('Paste shapefile name to use as mask and hit enter\n') 
 
-## Snap raster is raster with desired projection, resolution, and extent.
-## All other rasters in the set will be projected, resampled, and clipped to the snap raster
+## user chooses mask for clipping geometry
+mask=raw_input('Choose shapefile from list for clipping geometry\n') 
 
-cellsize=raw_input('Enter resample cell size and hit enter\n')
+## Describe cell size of snap raster
+cellsize = "{0} {1}".format(arcpy.Describe(snap).meanCellWidth, arcpy.Describe(snap).meanCellHeight)
 
 ## Project, resample, and clip rasters
 for raster in rasterlist:
     Ras=arcpy.Raster(raster)
-    projrast1=arcpy.ProjectRaster_management(Ras, "proj1", mask, "NEAREST")
+    arcpy.env.snapRaster = snap
+    projrast1=arcpy.ProjectRaster_management(Ras, "proj1", snap, "NEAREST")
     resamprast1=arcpy.Resample_management(projrast1, "resamp1", cellsize, "NEAREST")
     cliprast=arcpy.Clip_management(resamprast1, "#", raster+"_final", mask, "0", "ClippingGeometry")
     
